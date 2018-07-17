dd9024a432ed2926ae831b78b8eb75bd9e783b10
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-07-17 18:30:37 -0300

    create a boundaryCallback to update list and trigger network updates

--- app/src/main/java/com/nunes/eduardo/paging/data/GithubRepository.kt
+++ app/src/main/java/com/nunes/eduardo/paging/data/GithubRepository.kt
@@ -1,10 +1,8 @@
 package com.nunes.eduardo.paging.data
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.LivePagedListBuilder
 import android.util.Log
 import com.nunes.eduardo.paging.api.GithubService
-import com.nunes.eduardo.paging.api.searchRepos
 import com.nunes.eduardo.paging.db.GithubLocalCache
 import com.nunes.eduardo.paging.model.RepoSearchResult
 
@@ -15,16 +13,6 @@ class GithubRepository(
         private val service: GithubService,
         private val cache: GithubLocalCache
 ) {
-
-    // keep the last requested page. When the request is successful, increment the page number.
-    private var lastRequestedPage = 1
-
-    // LiveData of network errors.
-    private val networkErrors = MutableLiveData<String>()
-
-    // avoid triggering multiple requests in the same time
-    private var isRequestInProgress = false
-
     /**
      * Search repositories whose names match the query.
      */
@@ -34,33 +22,18 @@ class GithubRepository(
         // Get the data source factory from the local cache
         val dataSourceFactory = cache.reposByName(query)
 
+        //contruct the repoBoundaryCallback
+        val boundaryCallback = RepoBoundaryCallback(query, service, cache)
+
         //Get the paged List
-        val data = LivePagedListBuilder(dataSourceFactory, DATABASE_PAGE_SIZE).build()
+        val data = LivePagedListBuilder(dataSourceFactory, DATABASE_PAGE_SIZE)
+                .setBoundaryCallback(boundaryCallback)
+                .build()
 
-        return RepoSearchResult(data, networkErrors)
-    }
-
-    fun requestMore(query: String) {
-        requestAndSaveData(query)
-    }
-
-    private fun requestAndSaveData(query: String) {
-        if (isRequestInProgress) return
-
-        isRequestInProgress = true
-        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
-            cache.insert(repos) {
-                lastRequestedPage++
-                isRequestInProgress = false
-            }
-        }, { error ->
-            networkErrors.postValue(error)
-            isRequestInProgress = false
-        })
+        return RepoSearchResult(data, boundaryCallback.networkErrors)
     }
 
     companion object {
-        private const val NETWORK_PAGE_SIZE = 50
         private const val DATABASE_PAGE_SIZE = 20
     }
 }
---
+++ app/src/main/java/com/nunes/eduardo/paging/data/RepoBoundaryCallback.kt
@@ -0,0 +1,57 @@
+package com.nunes.eduardo.paging.data
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.paging.PagedList
+import com.nunes.eduardo.paging.api.GithubService
+import com.nunes.eduardo.paging.api.searchRepos
+import com.nunes.eduardo.paging.db.GithubLocalCache
+import com.nunes.eduardo.paging.model.Repo
+
+class RepoBoundaryCallback(
+        private val query: String,
+        private val service: GithubService,
+        private val cache: GithubLocalCache
+): PagedList.BoundaryCallback<Repo>() {
+
+    // keep the last requested page. When the request is successful, increment the page number.
+    private var lastRequestedPage = 1
+
+    // LiveData of network errors.
+    private val _networkErrors = MutableLiveData<String>()
+
+    val networkErrors: LiveData<String>
+        get() = _networkErrors
+
+    // avoid triggering multiple requests in the same time
+    private var isRequestInProgress = false
+
+
+    override fun onZeroItemsLoaded() {
+        requestAndSaveData(query)
+    }
+
+    override fun onItemAtEndLoaded(itemAtEnd: Repo) {
+        requestAndSaveData(query)
+    }
+
+    private fun requestAndSaveData(query: String) {
+        if (isRequestInProgress) return
+
+        isRequestInProgress = true
+        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
+            cache.insert(repos) {
+                lastRequestedPage++
+                isRequestInProgress = false
+            }
+        }, { error ->
+            _networkErrors.postValue(error)
+            isRequestInProgress = false
+        })
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 50
+
+    }
+}
